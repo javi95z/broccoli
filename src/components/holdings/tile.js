5af71415c23872a5ce4f73161fd32eb3e757f580
Javier Monfort <jmonfort@full-on-net.com>
2021-10-18 09:34:22 +0200

    Minor changes

--- src/components/holdings/tile.js
+++ src/components/holdings/tile.js
@@ -1,23 +1,36 @@
 import { cryptoFormat, currencyFormat, percentFormat } from "../../utils"
 import { BackgroundImage, CardRoot, SignFigure } from "../shared"
+import settings from "../../settings.json"
 
-const HoldingTile = ({ data }) => (
-  <CardRoot>
-    <div className="relative flex flex-col gap-6 w-full h-24 p-4 overflow-hidden">
-      <BackgroundImage image={data.image} width={50} height={50} />
-      <div className="flex justify-between">
-        <span className="text-lg font-medium">
-          {cryptoFormat(data.amount, data.symbol)}
-        </span>
-        <SignFigure
-          data={data.percentageDiff}
-          filter={percentFormat}
-          className="font-medium"
+const HoldingTile = ({ data }) => {
+  const link = data.id && `${settings.ROUTES.COINS}/${data.id}`
+
+  return (
+    <CardRoot>
+      <div className="relative flex flex-col gap-6 w-full h-24 p-4 overflow-hidden">
+        <BackgroundImage
+          image={data.image}
+          width={50}
+          height={50}
+          link={link}
         />
+
+        <div className="flex justify-between">
+          <span className="text-lg font-medium">
+            {cryptoFormat(data.amount, data.symbol)}
+          </span>
+          <SignFigure
+            data={data.percentageDiff}
+            filter={percentFormat}
+            className="font-medium"
+          />
+        </div>
+        <span className="text-gray-400 -mt-1">
+          {currencyFormat(data.price)}
+        </span>
       </div>
-      <span className="text-gray-400 -mt-1">{currencyFormat(data.price)}</span>
-    </div>
-  </CardRoot>
-)
+    </CardRoot>
+  )
+}
 
 export default HoldingTile
