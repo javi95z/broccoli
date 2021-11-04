f895f955c05f8ebf8c247d9671d49cd7b44125d1
Javier Monfort <javi.95.zgz@gmail.com>
2021-11-04 16:36:15 +0100

    Tailwind purge refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
   darkMode: false,
   theme: {
     extend: {}
