dc463ee57598ec8b8db4d29b040b6b98836eeb39
Javier Monfort <jmonfort@full-on-net.com>
2021-12-05 19:42:09 +0100

    Fix Tailwind custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false,
   theme: {
+    fontFamily: {
+      sans: "Poppins, Arial, sans-serif"
+    },
     extend: {}
   },
   variants: {
